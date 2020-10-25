87c9f91db52770463f6af729440a2e1b3fa20d76
Samy-great <66556638+Samy-great@users.noreply.github.com>
2020-10-25 14:12:37 +0530

    Traversing of a string
    traversing of a string

---
+++ traversing of string.py
@@ -0,0 +1,10 @@
+s=input("enter a string : ")
+for i in range (0,len(s)):
+    print(s[i],end="")
+print()   
+for i in s :
+    print(i,end="")
+print()
+for i in range (-len(s),0):
+    print(s[i],end="")
+
